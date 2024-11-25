510e2ed25e11c9e6243bcb5233ab5405182399fe
JC <120233340+justjuicenom@users.noreply.github.com>
2024-11-25 19:39:40 +1300

    Update index.js

--- index.js
+++ index.js
@@ -279,16 +279,18 @@ $(".btn_wrap").each(function (index) {
 // Swiper Slider
 const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
 
-const swiper = new Swiper(".swiper.swiper-marquee", {
-  slidesPerView: 'auto',
-  spaceBetween: '1.5rem',
-  loop: true,
-  speed: 4000,
-  allowTouchMove: false,
-  autoplay: prefersReducedMotion ? false : {
-    delay: 1,
-    disableOnInteraction: false,
-  },
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper-marquee', {
+    slidesPerView: 'auto',
+    spaceBetween: '1.5rem',
+    loop: true,
+    speed: 4000,
+    allowTouchMove: false,
+    autoplay: prefersReducedMotion ? false : {
+      delay: 1,
+      disableOnInteraction: false,
+    },
+  });
 });
 
 // Balance text
